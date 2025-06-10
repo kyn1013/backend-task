a7bd5455581d3c3d62cc1862b064e46d0447c146
kyn1013 <yn112511@naver.com>
2025-06-10 23:43:40 +0900

    Test : 관리자 전용 권한 추가 부여 테스트코드 작성 (#3)

--- src/test/java/com/example/backendtask/domain/user/controller/UserControllerTest.java
+++ src/test/java/com/example/backendtask/domain/user/controller/UserControllerTest.java
@@ -2,22 +2,24 @@ package com.example.backendtask.domain.user.controller;
 
 import com.example.backendtask.common.security.*;
 import com.example.backendtask.domain.auth.dto.AuthUser;
+import com.example.backendtask.domain.user.dto.request.RoleRequest;
 import com.example.backendtask.domain.user.entity.User;
 import com.example.backendtask.domain.user.enums.UserRole;
 import com.example.backendtask.domain.user.service.UserService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.sql.init.dependency.AbstractBeansOfTypeDatabaseInitializerDetector;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.mockito.ArgumentMatchers.refEq;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.authentication;
@@ -26,6 +28,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -66,7 +69,7 @@ class UserControllerTest {
     }
 
     @Test
-    public void 일반회원이_모든_회원정보를_조회하는데_실패하여_401에러가_뜬다() throws Exception {
+    public void 일반회원이_모든_회원정보를_조회하는데_실패하여_ACCESS_DENIED_에러가_뜬다() throws Exception {
         // given
         AuthUser authUser = new AuthUser("kyn@test.com", List.of(UserRole.ROLE_USER));
         JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
@@ -77,10 +80,57 @@ class UserControllerTest {
                         .with(authentication(authenticationToken)))
                 .andExpect(status().isForbidden())
                 .andExpect(jsonPath("$.name").value("ACCESS_DENIED"))
-                .andExpect(jsonPath("$.statusCode").value(HttpStatus.UNAUTHORIZED.value()))
+                .andExpect(jsonPath("$.statusCode").value(HttpStatus.FORBIDDEN.value()))
                 .andExpect(jsonPath("$.message").value("접근 권한이 없습니다."));
     }
 
+    @Test
+    public void 관리자가_일반회원에게_관리자_권한을_추가로_부여한다() throws Exception {
+        // given
+        AuthUser authUser = new AuthUser("kyn@test.com", List.of(UserRole.ROLE_ADMIN));
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
 
+        RoleRequest roleRequest = new RoleRequest();
+        ReflectionTestUtils.setField(roleRequest, "email", "minji@test.com");
+        ReflectionTestUtils.setField(roleRequest, "role", "ROLE_ADMIN");
+
+        User commonUser = User.of("minji@test.com", "0000", "minji", UserRole.ROLE_USER);
+        commonUser.addRole(UserRole.ROLE_ADMIN);
+
+        BDDMockito.given(userService.grantAdmin(refEq(roleRequest))).willReturn(commonUser);
+
+        // when & then
+        mockMvc.perform(patch("/api/v1/admin/roles")
+                        .with(authentication(authenticationToken))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(roleRequest)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.email").value("minji@test.com"))
+                .andExpect(jsonPath("$.password").value("0000"))
+                .andExpect(jsonPath("$.nickName").value("minji"))
+                .andExpect(jsonPath("$.userRoles[0]").value("ROLE_ADMIN"))
+                .andExpect(jsonPath("$.userRoles[1]").value("ROLE_USER"));
+    }
+
+    @Test
+    public void 일반회원이_권한을_부여하는데_실패하여_ACCESS_DENIED_에러가_뜬다() throws Exception {
+        // given
+        AuthUser authUser = new AuthUser("kyn@test.com", List.of(UserRole.ROLE_USER));
+        JwtAuthenticationToken authenticationToken = new JwtAuthenticationToken(authUser);
+
+        RoleRequest roleRequest = new RoleRequest();
+        ReflectionTestUtils.setField(roleRequest, "email", "minji@test.com");
+        ReflectionTestUtils.setField(roleRequest, "role", "ROLE_ADMIN");
+
+        // when & then
+        mockMvc.perform(patch("/api/v1/admin/roles")
+                        .with(authentication(authenticationToken))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(roleRequest)))
+                .andExpect(status().isForbidden())
+                .andExpect(jsonPath("$.name").value("ACCESS_DENIED"))
+                .andExpect(jsonPath("$.statusCode").value(HttpStatus.FORBIDDEN.value()))
+                .andExpect(jsonPath("$.message").value("접근 권한이 없습니다."));
+    }
 
 }
